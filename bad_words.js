a8ff46923226c6a68fa691a3ca33724e217fe267
itay opl <itay.opl@gmail.com>
2020-10-08 23:54:29 +0300

    created server

---
+++ bad_words.js
@@ -0,0 +1,5 @@
+const hasBadWords = (title, message) => {
+    
+};
+
+module.exports = hasBadWords;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const hasBadWords = require('./bad_words');
+
+const app = express();
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(Object.values(posts));
+});
+
+app.post('/', (req, res) => {
+    const title = req.body.title;
+    const message = req.body.message;
+    const id = uuidv4();
+    let containsBadWords = hasBadWords(title, message);
+
+    const post = {
+        title,
+        message,
+        id,
+        containsBadWords
+    };
+
+    posts[id] = post;
+    res.send(post);
+});
+
+app.listen(3000, () => {
+    console.log("listening");
+});
